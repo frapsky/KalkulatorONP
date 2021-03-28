da28cd3fe49d3ed701f8e01a7cc32df496c39beb
frapsky <jakubszostk@gmail.com>
2021-03-28 18:55:44 +0200

    Working calculator, added operations

--- src/js/company/Main.java
+++ src/js/company/Main.java
@@ -1,8 +1,71 @@
 package js.company;
+import java.util.Scanner;
+import java.util.Stack;
+
+
 
 public class Main {
 
+    static Stack stack = new Stack();
+
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        String wynik = scanner.nextLine();
+
+        Stack<Double> values = new Stack<>(); //stos
+
+        String[] tokens = wynik.split(" +");
+        double answer = 0.0;
+
+        for (String token : tokens)//for each loop, dla kazdego tokena w tablicy tokenow...
+        {
+            if(!isOperator(token))//kiedy zmienna -> dodaj na stos
+            {
+                values.add(Double.parseDouble(token));
+                //System.out.println(token);
+            }
+            else// sciagnij  ze stosu by wykonac operacje
+            {
+                double a = values.pop();
+                double b = values.pop();
+                switch(token)
+                {
+                    case "+":
+                        answer = a + b;
+                        break;
+                    case "-":
+                        answer = b - a;
+                        break;
+                    case "*":
+                        answer = a * b;
+                        break;
+                    case "/":
+                        answer = a / b;
+                        break;
+                    case "^":
+                        answer = Math.pow(a,b);
+                        break;
+                    case "|": //sqrt a^b
+                        answer = Math.pow(a,1/b);
+                        break;
+
+                }
+                values.add(answer);// dodaj na stos wynik
+                //System.out.println(answer);
+            }
+        }
+        while(true){
+            if(values.empty())
+                break;
+            else
+                System.out.println(values.pop()); //tutaj taka proba wypisania na koncu
+        }
+
+    }
+
+    private static boolean isOperator(String s)
+    {
+        return s.equals("+") || s.equals("-") || s.equals("*") || s.equals("/") || s.equals("^") || s.equals("|");
     }
 }
